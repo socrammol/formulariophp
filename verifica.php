e63046d8337a750a6e20e126473a5030db08c8ec
Marcos Mol <marcos_mol@hotmail.com>
2019-04-04 00:33:35 -0300

    validando data

--- verifica.php
+++ verifica.php
@@ -5,11 +5,25 @@
         $Texto        = $_POST["Texto"];
         $Textogrande  = $_POST["Textogrande"];
         $erro         = 0;
-        function validarData($Data) {
-            if (!eregi("^[0-9]{2}-[0-9]{2}-[0-9]{4}$", $Data)) { 
-                echo "data invalida";
+## •Data: deverá ser um campo de data no seguinte formato mm-dd-YYYY ok
+##• Texto: O texto só deverá possuir letras minúsculas e espaços, até 144 chars.
+##• Texto grande: O texto só deverá possuir letras maiúsculas, números e espaços até 255 chars.
+        //criando um array
+        $array = explode('-', $Data);
+        //garante que o array possue tres elementos (dia, mes e ano)
+        if(count($array) == 3){
+            $dia = (int)$array[1];
+            $mes = (int)$array[0];
+            $ano = (int)$array[2];
+            //testa se a data é válida
+            if(checkdate($mes, $dia, $ano)){
+                echo "data '$Data' e valida'";
+            }else{
+                echo "data '$Data' e invalida";
             }
+        }else{
+            echo "formato da data '$Data' invalido";
         }
-?>
+        ?>
     </body>
 </html>
