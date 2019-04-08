b06099d1807d53933c505e2323b5bd11fd48360b
LeonardoMarco <leo26alves@yahoo.com.br>
2019-04-08 14:16:02 -0300

    index estilizada

--- fragment/header.php
+++ fragment/header.php
@@ -4,6 +4,6 @@
     <title>Formulario de teste</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" href="css/estilo.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     </head>
--- index.php
+++ index.php
@@ -1,24 +1,40 @@
 <?php require_once('fragment/header.php'); ?>
+
 <body>
-     <div class="conatiner">
-          <div class="head">
-               <h1 class="h1">Formulário de teste</h1>
-          </div>
-          <div class='br'>
-          </div>
-          <div class="formulario">
-               <form action="UI_Comp_Formulario.php"  name="form1" method="post">
-                    <div class="form">
-                         <pre>
-                              Data:        <input type="text" size="20" maxlength="256" name="Data" id="data">
-                              Texto:       <input type="text" size="20" maxlength="256" name="Texto" id="texto">
-                              Checkbox:    <input type="checkbox"  name="Checkbox">
-                              Textogrande: <textarea rows="5" cols="52" name="Textogrande" id="textogrande"></textarea>
-                              <input type="submit" value="submit" name="submit" onclick="return validate()" >
-                         </pre>
+    <div class="conatiner">
+        <div class="head">
+            <h1 class="h1">Formulário de teste</h1>
+        </div>
+        <div class="formulario">
+            <form action="UI_Comp_Formulario.php" name="form1" method="post">
+                <div class="form">
+                    <div>
+                        <label for="data">Data:</label>
+                        <input type="text" size="20" maxlength="255" name="data" id="data">
                     </div>
-               </form>
-          </div>
-     </div>
+
+                    <div>
+                        <label for="texto">Texto:</label>
+                        <input type="text" size="20" maxlength="256" name="texto" id="texto">
+                    </div>
+
+                    <div>
+                        <label for="checkbox">Checkbox:</label>
+                        <input type="checkbox" name="Checkbox">
+                    </div>
+
+                    <div>
+                         <label for="textogrande" class="label-area">Textogrande:</label>
+                        <textarea rows="8" cols="40" name="Textogrande" id="textogrande"></textarea>
+                    </div>
+
+                    <input type="submit" value="Submit" name="submit" onclick="return validate()">
+                </div>
+                <div class="desc-form">
+                     Teste de formulário
+                </div>
+            </form>
+        </div>
+    </div>
 </body>
 <?php require_once('fragment/footer.php'); ?>
